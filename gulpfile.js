e7ad894d216c150b4ced4d8c29c046a9f8265e34
Tobira <tobirawork@gmail.com>
2017-05-13 10:47:48 +0300

    added task img

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,15 @@
 const gulp = require('gulp');
+//fot styles
 const stylus = require('gulp-stylus');
 const gcmq = require('gulp-group-css-media-queries');
 const autoprefixer = require('gulp-autoprefixer');
+//local server and reload
 const browserSync = require('browser-sync');
-
-const config = {
+//for images
+const imagemin = require('gulp-imagemin');
+const pngQuant = require('imagemin-pngquant');
+//path config
+const paths = {
     src: 'src/',
 
     css: {
@@ -14,21 +19,32 @@ const config = {
 
     html: {
         src: '*.html'
+    },
+
+    img: {
+        src: 'img/**/*'
     }
 };
 
 gulp.task('stylus', () => {
-    return gulp.src(config.src + config.css.watch)
+    return gulp.src(paths.src + paths.css.watch)
         .pipe(stylus())
         .pipe(autoprefixer({
             browsers: ['> 0.01%'],
             cascade: false
         }))
         .pipe(gcmq())
-        .pipe(gulp.dest(config.src + config.css.dest))
+        .pipe(gulp.dest(paths.src + paths.css.dest))
         .pipe(browserSync.reload({stream: true}));
 });
 
 gulp.task('img', () => {
-
+    return gulp.src(paths.src + paths.img.src)
+        .pipe(imagemin({
+            interlaced: true,
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngQuant()]
+        }))
+        .pipe(gulp.dest('dist/img'));
 });
