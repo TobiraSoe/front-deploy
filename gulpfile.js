5a4965d8541b6019222264e716f07c0d2ce3a585
Tobira <tobirawork@gmail.com>
2017-05-13 11:04:25 +0300

    added task del and watch

--- gulpfile.js
+++ gulpfile.js
@@ -8,13 +8,19 @@ const browserSync = require('browser-sync');
 //for images
 const imagemin = require('gulp-imagemin');
 const pngQuant = require('imagemin-pngquant');
+//other
+const del = require('del');
 //path config
 const paths = {
+    build: 'build/',
     src: 'src/',
 
+    js: {
+        src: 'js/**/*.js'
+    },
+
     css: {
-        watch: 'stylus/**/*.styl',
-        dest: 'css'
+        src: 'stylus/**/*.styl',
     },
 
     html: {
@@ -27,14 +33,14 @@ const paths = {
 };
 
 gulp.task('stylus', () => {
-    return gulp.src(paths.src + paths.css.watch)
+    return gulp.src(paths.src + paths.css.src)
         .pipe(stylus())
         .pipe(autoprefixer({
             browsers: ['> 0.01%'],
             cascade: false
         }))
         .pipe(gcmq())
-        .pipe(gulp.dest(paths.src + paths.css.dest))
+        .pipe(gulp.dest(paths.src + 'css'))
         .pipe(browserSync.reload({stream: true}));
 });
 
@@ -49,3 +55,21 @@ gulp.task('img', () => {
         .pipe(gulp.dest('dist/img'));
 });
 
+gulp.task('browser-sync', () => {
+    browserSync({
+        server: {baseDir: paths.src},
+        notify: false
+    });
+});
+
+gulp.task('watch', ['stylus', 'browser-sync'], () => {
+    gulp.watch(paths.src + paths.css.watch, ['styl']);
+    gulp.watch(paths.src + '/**/' + paths.html.src, browserSync.reload);
+    gulp.watch(paths.src + paths.js.src, browserSync.reload);
+});
+
+gulp.task('clean-build-dir', function() {
+    return del(paths.build);
+});
+
+gulp.task('default', ['watch']);
